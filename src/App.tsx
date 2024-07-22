3277862112733106e69292c11db4ce54cadf9279
abuzar <mamedov@mercurydevelopment.com>
2024-07-22 03:34:45 +0300

    init 6

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import { useTelegram } from './hooks/useTelegram';
+import Header from './components/Header/Header';
 
 const App = () => {
   const { tg, onToggleButton } = useTelegram();
@@ -10,6 +11,7 @@ const App = () => {
 
   return (
     <div>
+      <Header />
       <button onClick={onToggleButton}>Toggle</button>
     </div>
   );
